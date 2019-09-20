160f9cb62435a31a06532c25bb038c541d797391
Ajaypal Cheema <ajaypalster@gmail.com>
2019-09-20 17:20:39 -0400

    Updates

---
+++ precache-manifest.82bf63b7f4c5923adb2ed9531240710e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6982c62670dc93171618",
+    "url": "/redux-converter/static/js/runtime~main.0db10f0a.js"
+  },
+  {
+    "revision": "c016f63db667f2ae7209",
+    "url": "/redux-converter/static/js/main.a1eb9e38.chunk.js"
+  },
+  {
+    "revision": "aab3958f37ef26dc1572",
+    "url": "/redux-converter/static/js/2.c1a5bf5b.chunk.js"
+  },
+  {
+    "revision": "c016f63db667f2ae7209",
+    "url": "/redux-converter/static/css/main.ba57d5b4.chunk.css"
+  },
+  {
+    "revision": "4156331bc7f6e26e9844ec9e5f56956f",
+    "url": "/redux-converter/index.html"
+  }
+];
