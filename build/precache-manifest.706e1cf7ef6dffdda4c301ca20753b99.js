2602a16497c7be2fe200cced4de393913ea87543
Ajaypal Cheema <ajaypalster@gmail.com>
2019-09-20 17:21:49 -0400

    fux config and build

---
+++ build/precache-manifest.706e1cf7ef6dffdda4c301ca20753b99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0078c8eb852a8f895f9",
+    "url": "/redux-counter/static/js/runtime~main.125d2423.js"
+  },
+  {
+    "revision": "c016f63db667f2ae7209",
+    "url": "/redux-counter/static/js/main.a1eb9e38.chunk.js"
+  },
+  {
+    "revision": "aab3958f37ef26dc1572",
+    "url": "/redux-counter/static/js/2.c1a5bf5b.chunk.js"
+  },
+  {
+    "revision": "c016f63db667f2ae7209",
+    "url": "/redux-counter/static/css/main.ba57d5b4.chunk.css"
+  },
+  {
+    "revision": "dcf8b51f7adf6c39a0cd4f389827d19a",
+    "url": "/redux-counter/index.html"
+  }
+];
